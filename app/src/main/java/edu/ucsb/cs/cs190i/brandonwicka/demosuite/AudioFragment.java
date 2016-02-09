d856dee056eedea7d1e376a9ea5527daf6f3b69a
brandonwicka <brandonwicka@umail.ucsb.edu>
2016-02-09 15:41:17 -0800

    Implemented audio button, fully working

--- app/src/main/java/edu/ucsb/cs/cs190i/brandonwicka/demosuite/AudioFragment.java
+++ app/src/main/java/edu/ucsb/cs/cs190i/brandonwicka/demosuite/AudioFragment.java
@@ -1,18 +1,23 @@
 package edu.ucsb.cs.cs190i.brandonwicka.demosuite;
 
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class AudioFragment extends Fragment {
+public class AudioFragment extends Fragment implements View.OnClickListener {
 
+    Button button;
 
     public AudioFragment() {
         // Required empty public constructor
@@ -23,7 +28,20 @@ public class AudioFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_audio, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_audio, container, false);
+        button = (Button) view.findViewById(R.id.play_audio_button);
+        button.setOnClickListener(this);
+        return view;
     }
+
+
+    @Override
+    public void onClick(View v) {
+        MediaPlayer mp = MediaPlayer.create(getContext(), R.raw.fanfare);
+        mp.start();
+
+    }
+
 
 }
