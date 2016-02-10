adbdca8c5d6ea0e096b381ea9bb82c9d1e5b07fe
brandonwicka <brandonwicka@umail.ucsb.edu>
2016-02-09 18:13:16 -0800

    Working implementations of Text-to-speech and Speech-to-text buttons

--- app/src/main/java/edu/ucsb/cs/cs190i/brandonwicka/demosuite/SpeechToTextFragment.java
+++ app/src/main/java/edu/ucsb/cs/cs190i/brandonwicka/demosuite/SpeechToTextFragment.java
@@ -1,17 +1,34 @@
 package edu.ucsb.cs.cs190i.brandonwicka.demosuite;
 
 
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SpeechToTextFragment extends Fragment {
+public class SpeechToTextFragment extends Fragment implements View.OnClickListener {
+
+
+    private final int REQ_CODE_SPEECH_INPUT = 1001;
+    public ImageButton micButton;
+    public TextView textSpeech;
 
 
     public SpeechToTextFragment() {
@@ -22,8 +39,46 @@ public class SpeechToTextFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_speech_to_text, container, false);
+        View view = inflater.inflate(R.layout.fragment_speech_to_text, container, false);
+        micButton = (ImageButton) view.findViewById(R.id.img_button);
+        micButton.setOnClickListener(this);
+        textSpeech = (TextView) view.findViewById(R.id.text_speak);
+
+        return view;
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == getActivity().RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    textSpeech.setText(result.get(0));
+                }
+                break;
+            }
+
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(getActivity().getApplicationContext(), "Not Supported", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+
+
 
 }
--- app/src/main/java/edu/ucsb/cs/cs190i/brandonwicka/demosuite/TextToSpeechFragment.java
+++ app/src/main/java/edu/ucsb/cs/cs190i/brandonwicka/demosuite/TextToSpeechFragment.java
@@ -1,18 +1,29 @@
 package edu.ucsb.cs.cs190i.brandonwicka.demosuite;
 
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.Locale;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class TextToSpeechFragment extends Fragment {
+public class TextToSpeechFragment extends Fragment implements View.OnClickListener, TextToSpeech.OnInitListener {
 
+
+    TextToSpeech engine;
+    EditText editText;
 
     public TextToSpeechFragment() {
         // Required empty public constructor
@@ -22,8 +33,36 @@ public class TextToSpeechFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_text_to_speech, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_text_to_speech, container, false);
+        Button speakButton = (Button) view.findViewById(R.id.speak_button);
+        speakButton.setOnClickListener(this);
+        editText = (EditText) view.findViewById(R.id.edit_text);
+        editText.getBackground().setColorFilter(Color.parseColor("#ff69b4"),PorterDuff.Mode.SRC_IN);
+        engine = new TextToSpeech(getActivity().getApplicationContext(), this);
+
+        return view;
     }
 
+
+
+    @Override
+    public void onClick(View v) {
+        speech();
+    }
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            engine.setLanguage(Locale.US);
+        }
+    }
+
+
+    public void speech() {
+        engine.setPitch((float) 1.0);
+        engine.setSpeechRate((float) 1.0);
+        engine.speak(editText.getText().toString(), TextToSpeech.QUEUE_FLUSH, null, null);
+
+    }
 }
